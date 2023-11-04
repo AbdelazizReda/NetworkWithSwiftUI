6f1805c25d7af9d607decd1873e0a2d1cdb27915
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2023-11-04 03:52:44 +0200

    test
    test

--- NetworkWithSwiftUI/ContentView.swift
+++ NetworkWithSwiftUI/ContentView.swift
@@ -19,7 +19,9 @@ struct ContentView: View {
 
             } else {
                 Text("Data Loading.... ")
+                Text("Data Loading.... ")
 
+                Text("Data Loading.... ")
                     .onAppear {
                         viewModel.fetchData()
                     }
